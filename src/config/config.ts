749f05117e57e7d684976ddf20b90b3ac169004c
FrancoEspinozaV <franco.espinoza.val@gmail.com>
2024-12-13 21:51:21 -0300

    only one call to process.env

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,9 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
 
-export const PORT = process.env.PORT || 3000
-export const MONGO_URI = process.env.MONGO_URI || ''
-export const RESEND_API_KEY = process.env.API_KEY_RESEND || ''
-export const JWT_SECRET = process.env.JWT_SECRET || 'SecretToken'
+export const {
+  PORT = 3000,
+  MONGO_URI = '',
+  API_KEY_RESEND = '',
+  JWT_SECRET = 'SecretToken',
+} = process.env
